67d454c8efab28ef9bbae51c7a1b0f0a54393ffd
Wilsona <alex@wilsona.xyz>
2024-04-21 16:49:05 +0300

    Complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0){
+        return 'ERROR';
+    }
+    let complete = "";
+    for (let i=0; i<num; i++){
+        complete += string;
+    }
+    return complete;
 };
 
 // Do not edit below this line
