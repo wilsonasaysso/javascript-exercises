a8fe308deaaeaa4b854c8139080763a559abe85e
Wilsona <alex@wilsona.xyz>
2024-04-22 17:23:04 +0300

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  let temp = (num - 32) * (5/9);
+  let rounded = Math.round(temp * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  let temp = ((num * 9) / 5 + 32);
+  let rounded = Math.round(temp * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
