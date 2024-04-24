05d852e17f0743ba695713f214d57dfbdf5915e0
Wilsona <alex@wilsona.xyz>
2024-04-24 13:39:36 +0300

    Complete 09 10 11

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const string = str.replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    const stringNew = string.toLowerCase();
+    const word = stringNew.split('');
+    const wordReverse = word.reverse();
 
+    for (let i=0; i < word.length; i++){
+        for (let j=0; j < wordReverse.length; j++){
+            if (word[i] != wordReverse[j]) {
+                return false;
+            } else continue;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
